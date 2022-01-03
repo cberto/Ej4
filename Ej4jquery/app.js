c8594307ad917cc7abbb31477ffb9f61b263ad38
cberto <bertolanicami@gmail.com>
2022-01-03 17:52:42 -0300

    first commit

---
+++ Ej4jquery/app.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function() {
+    $("#btn1").click(function() {
+        $.get("data.txt", function (data) {
+            alert("Metodos Ajax: ");
+            document.getElementById("texto1").value = "Cargando con Jquery: " + data;
+            });
+       });
+       $("#btn2").click(function () {
+        $.post("test.ashx" ,
+                 {
+                     param1: $("[name = nombre]").val(),
+                     param2: $("[name = apellido]").val()
+                  },
+                    function (data) {
+                        alert("Utilizando metodos Ajax; ");
+                        crearContainer(data);
+                        });
+        });
+    });
+    function crearContainer (texto) {
+        var t = document.createTextNode(texto);
+        var h = document.createElement("span");
+        h.appendChild(t);
+        var container = document.getElementById("dataPersonal");
+        container.innerHTML = null;
+        container.appendChild(h);
+        
+    };
---
+++ Ej4jquery/test.ashx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ej4jquery
+{
+    /// <summary>
+    /// Descripción breve de test
+    /// </summary>
+    public class Test : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            string param1 = context.Request["param1"].ToString();
+            string param2 = context.Request["param2"].ToString();
+
+            string saludo = string.Format("Hola {0} {1}", param1, param2);
+            context.Response.Write(saludo);
+
+        }
+
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
